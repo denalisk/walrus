e4348dd3e47a80bb9dff5444bfe115bf14d72436
Sam Kirsch and Shane Villars <shane@>
2017-01-10 09:36:08 -0800

    add clickable buttons to change body

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,19 @@ $(document).ready(function() {
     $(".iceberg").slideToggle();
   });
 
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
 });
